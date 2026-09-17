f89aac197a15bb2b4b5d6e31f494d3b77b2e33e0
geohumaranavitia-beep <geohumaranavitia@gmail.com>
2026-09-17 14:33:56 -0600

    Add files via upload
    # 🚓 Sistema Estroboscópico de Emergencia Tipo Patrulla (V1.0)
    
    ¡Hola! Este proyecto forma parte de mi portafolio de robótica y sistemas embebidos, desarrollado de forma independiente durante mi tercer semestre de preparatoria en Durango, México.
    
    El objetivo de este circuito es diseñar un sistema de señalización de emergencia con conmutación de energía a alta velocidad, simulando los destellos intermitentes de una patrulla policial utilizando la placa **Arduino Uno**.
    
    ---
    
    ## 🛠️ Hardware Utilizado (Simulado en Tinkercad)
    * **1 Placa Arduino Uno R3** (Microcontrolador central)
    * **1 Placa de pruebas pequeña** (Protoboard)
    * **3 LEDs de colores:** 
      * LED Verde (Izquierdo / Pin 13)
      * LED Amarillo (Centro / Pin 12 - Configurado como nodo muerto/apagado)
      * LED Rojo (Derecho / Pin 11)
    * **3 Resistencias de 220 Ω** (Calibradas con código de franjas *Rojo, Rojo, Café* para permitir el paso de la máxima potencia eléctrica y brillo gráfico sin dañar los componentes)
    * **Cableado de puenteo hacia masa general (GND)**
    
    ---
    
    ## 🧠 Arquitectura del Software (C++)
    El código fuente fue desarrollado a mano y optimizado para evitar "fantasmas eléctricos" (luces que se quedan encendidas por error al reiniciar el ciclo). Aplica los siguientes conceptos:
    
    1. **Configuración de Salidas (`pinMode`):** Apertura de compuertas digitales de los pines 13, 12 y 11 en modo `OUTPUT` para inyectar 5 voltios limpios a las columnas.
    2. **Conmutación Síncrona Continua:** Dentro del bucle infinito `void loop()`, el sistema intercambia de forma inmediata los estados lógicos (`HIGH` y `LOW`) entre el extremo izquierdo y el derecho.
    3. **Control de Tiempos Críticos (`delay`):** Los retardos se calibraron a apenas **150 milisegundos**, logrando una frecuencia de parpadeo ultra veloz y llamativa que responde de forma fluida en el simulador.
    
    ---
    *Meta Profesional: Continuar acumulando proyectos modulares en mi perfil público para postular a la Ingeniería en Sistemas Computacionales en el Instituto Tecnológico de Durango (ITD) y abrirme camino hacia la industria de software internacional.*

---
+++ brilliant_juttuli1.ino
@@ -0,0 +1,22 @@
+void setup (){
+pinMode(13, OUTPUT);
+pinMode(12, OUTPUT);
+pinMode(11, OUTPUT);
+
+}
+
+void loop () {
+
+digitalWrite(12, LOW);
+
+
+digitalWrite(13, HIGH);
+digitalWrite(11, LOW);
+delay(150);
+
+
+digitalWrite(13, LOW);
+digitalWrite(11, HIGH);
+delay(150);
+
+}
